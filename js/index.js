353f09de6677252f85abeeedf0edf5584fbb871b
Angel Barajas <Barajasb15@gmail.com>
2022-09-07 12:28:51 -0500

    add: fondo

--- js/index.js
+++ js/index.js
@@ -10,53 +10,7 @@ let anim = document.getElementById("animation");
 lottie.loadAnimation({
     wrapper: anim, // the dom element that will contain the animation
     animType: 'svg',
-    loop: false,
+    loop: true,
     autoplay: true,
-    path: '../json/man.json' // the path to the animation json
-});
-
-let anim2 = document.getElementById("animation2");
-
-lottie.loadAnimation({
-    wrapper: anim2, // the dom element that will contain the animation
-    animType: 'svg',
-    loop: false,
-    autoplay: true,
-    path: '../json/women.json' // the path to the animation json
-});
-let anim3 = document.getElementById("animation3");
-
-lottie.loadAnimation({
-    wrapper: anim3, // the dom element that will contain the animation
-    animType: 'svg',
-    loop: false,
-    autoplay: true,
-    path: '../json/boy.json' // the path to the animation json
-});
-let anim4 = document.getElementById("animation4");
-
-lottie.loadAnimation({
-    wrapper: anim4, // the dom element that will contain the animation
-    animType: 'svg',
-    loop: false,
-    autoplay: true,
-    path: '../json/cowgirl.json' // the path to the animation json
-});
-let anim5 = document.getElementById("animation5");
-
-lottie.loadAnimation({
-    wrapper: anim5, // the dom element that will contain the animation
-    animType: 'svg',
-    loop: false,
-    autoplay: true,
-    path: '../json/pinky.json' // the path to the animation json
-});
-let anim6 = document.getElementById("animation6");
-
-lottie.loadAnimation({
-    wrapper: anim6, // the dom element that will contain the animation
-    animType: 'svg',
-    loop: false,
-    autoplay: true,
-    path: '../json/mouse.json' // the path to the animation json
+    path: '../json/InicioAnim.json' // the path to the animation json
 });
